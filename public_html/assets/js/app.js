0369d7622874fe6d883c5e998a786a614018cfd8
randyluna222 <108312411+randyluna222@users.noreply.github.com>
2023-11-23 21:49:45 -0600

    first commit

---
+++ public_html/assets/js/app.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+// protfolio filters
+$(window).on("load", function() {
+    var t = $(".portfolio-container");
+    t.isotope({
+        filter: ".new",
+        animationOptions: {
+            duration: 750,
+            easing: "linear",
+            queue: !1
+        }
+    }), $(".filters a").click(function() {
+        $(".filters .active").removeClass("active"), $(this).addClass("active");
+        var i = $(this).attr("data-filter");
+        return t.isotope({
+            filter: i,
+            animationOptions: {
+                duration: 750,
+                easing: "linear",
+                queue: !1
+            }
+        }), !1
+    });
+});
+
+//Ver PDF
+document.getElementById('descargarPDF').addEventListener('click', function() {
+  // Cambia esta URL por la del archivo PDF alojado en tu servidor
+  var url = 'https://www.dropbox.com/scl/fi/eysrr221hanrezl3nrn2e/CV-Randy-Luna-Ram-rez.pdf?rlkey=slvdcvxrt5gw5zyvdu0ms16kb&dl=0';
+
+  var enlace = document.createElement('a');
+  enlace.href = url;
+  enlace.download = 'nombre-del-archivo.pdf';
+
+  document.body.appendChild(enlace);
+
+  enlace.click();
+
+  document.body.removeChild(enlace);
+});
+
